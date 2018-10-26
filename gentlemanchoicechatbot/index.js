ca169961ba0e2c03d5762ad695cae0cdfb0e2ec4
mroiseau <nikolabijanic93@gmail.com>
2018-10-26 20:25:44 +0200

    Token

--- gentlemanchoicechatbot/index.js
+++ gentlemanchoicechatbot/index.js
@@ -3,6 +3,7 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const request = require('request'); //dodao 
+const _ = require('loadash');
 
 const app = express();
 
